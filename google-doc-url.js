2f2879ff14201b26491a0fa50e19f88f64c9a40b
Rick Frantz <rick-frantz@users.noreply.github.com>
2017-05-16 13:59:50 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Yv-FMpNLigBDJUue88U6w42vD_OjkoTeCXH93QNnFkM/edit#gid=0';
